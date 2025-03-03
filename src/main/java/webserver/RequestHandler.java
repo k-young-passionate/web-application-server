b07371b52d0ba348fe7033b80ef489a514d664a3
k-young-passionate <kyshim9004@gmail.com>
2025-03-03 18:44:11 +0900

    feat(요구사항 1): response requested files

--- src/main/java/webserver/RequestHandler.java
+++ src/main/java/webserver/RequestHandler.java
@@ -1,10 +1,14 @@
 package webserver;
 
+import java.io.BufferedReader;
 import java.io.DataOutputStream;
+import java.io.File;
 import java.io.IOException;
 import java.io.InputStream;
+import java.io.InputStreamReader;
 import java.io.OutputStream;
 import java.net.Socket;
+import java.nio.file.Files;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -23,20 +27,85 @@ public class RequestHandler extends Thread {
                 connection.getPort());
 
         try (InputStream in = connection.getInputStream(); OutputStream out = connection.getOutputStream()) {
-            // TODO 사용자 요청에 대한 처리는 이 곳에 구현하면 된다.
+            String path = extractPath(in);
+
+            String contentType = "html";
+            if (path.endsWith(".css")) {
+                contentType = "css";
+            } else if (path.endsWith(".js")) {
+                contentType = "js";
+            }
+
+            byte[] body = readFile(path);
+
+            if (body == null) {
+                body = "Hello World".getBytes();
+            }
+
             DataOutputStream dos = new DataOutputStream(out);
-            byte[] body = "Hello World".getBytes();
-            response200Header(dos, body.length);
+
+            response200Header(dos, body.length, contentType);
             responseBody(dos, body);
         } catch (IOException e) {
             log.error(e.getMessage());
         }
     }
 
-    private void response200Header(DataOutputStream dos, int lengthOfBodyContent) {
+    private String extractMethod(InputStream in) {
+        return parseRequest(in, ParseResource.METHOD);
+    }
+
+    private String extractPath(InputStream in) {
+        return parseRequest(in, ParseResource.PATH);
+    }
+
+    enum ParseResource {
+        METHOD(0), PATH(1);
+
+        private final int position;
+
+        ParseResource(int position) {
+            this.position = position;
+        }
+
+        public int getPosition() {
+            return position;
+        }
+    }
+    
+    private String parseRequest(InputStream in, ParseResource resource) {
+        InputStreamReader isr = new InputStreamReader(in);
+        BufferedReader br = new BufferedReader(isr);
+
+        String line = null;
+        try {
+            line = br.readLine();
+            String[] splited = line.split(" ");
+            return splited[resource.getPosition()];
+        } catch (IOException e) {
+            log.error(e.getMessage());
+        }
+        return "";
+    }
+
+    private byte[] readFile(String fileName) {
+        byte[] body = null;
+        try {
+            body = Files.readAllBytes(new File("./webapp" + fileName).toPath());
+        } catch (IOException e) {
+            log.error(e.getMessage());
+        }
+        return body;
+    }
+
+    private void response200Header(DataOutputStream dos, int lengthOfBodyContent, String contentType) {
+        if (contentType == null) {
+            contentType = "html";
+        }
+
         try {
             dos.writeBytes("HTTP/1.1 200 OK \r\n");
-            dos.writeBytes("Content-Type: text/html;charset=utf-8\r\n");
+            dos.writeBytes("Content-Type: text/" + contentType + ";charset=utf-8\r\n");
             dos.writeBytes("Content-Length: " + lengthOfBodyContent + "\r\n");
             dos.writeBytes("\r\n");
         } catch (IOException e) {
